6a50b66d5678644e314f889957c099fddda680b0
Data-Bishop <nkanangabasifreke.data@gmail.com>
2025-04-14 22:04:59 +0100

    Formatted Terraform Code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,7 +1,7 @@
 module "s3" {
-  source             = "./modules/s3"
-  data_bucket_name   = var.data_bucket_name
+  source              = "./modules/s3"
+  data_bucket_name    = var.data_bucket_name
   airflow_bucket_name = var.airflow_bucket_name
-  logs_bucket_name   = var.logs_bucket_name
-  project_name       = var.project_name
+  logs_bucket_name    = var.logs_bucket_name
+  project_name        = var.project_name
 }
