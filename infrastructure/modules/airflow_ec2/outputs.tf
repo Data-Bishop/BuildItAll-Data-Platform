1c0bb64a7413e3e494e261824fabfbdc85361685
Data-Bishop <nkanangabasifreke.data@gmail.com>
2025-04-16 19:47:08 +0100

    Updated infra to output public and private ips

--- infrastructure/modules/airflow_ec2/outputs.tf
+++ infrastructure/modules/airflow_ec2/outputs.tf
@@ -1,5 +1,9 @@
-# modules/airflow_ec2/outputs.tf
 output "airflow_role_arn" {
   description = "ARN of the Airflow IAM role"
   value       = aws_iam_role.airflow_role.arn
+}
+
+output "airflow_private_ip" {
+  description = "Private IP of the Airflow EC2 instance"
+  value       = aws_instance.airflow.private_ip
 }
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -9,3 +9,13 @@ output "airflow_bucket_arn" {
 output "logs_bucket_arn" {
   value = module.s3.logs_bucket_arn
 }
+
+output "airflow_private_ip" {
+  description = "Private IP of the Airflow EC2 instance"
+  value       = module.airflow_ec2.airflow_private_ip
+}
+
+output "bastion_public_ip" {
+  description = "Public IP of the bastion host"
+  value       = module.bastion.bastion_public_ip
+}
