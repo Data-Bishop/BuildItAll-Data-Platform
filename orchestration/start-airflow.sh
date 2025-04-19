9c006a6f5a228fb7ad31542bd791d13487b48bf2
Data-Bishop <nkanangabasifreke.data@gmail.com>
2025-04-19 22:42:31 +0100

    Updated docker compose and Airflow setup

--- orchestration/start-airflow.sh
+++ orchestration/start-airflow.sh
@@ -5,5 +5,10 @@ cd /home/ec2-user/airflow
 # Download the Docker Compose file from S3
 aws s3 cp s3://builditall-airflow/docker-compose.yml /home/ec2-user/airflow/docker-compose.yml
 
+# Set the Airflow UID
+echo -e "AIRFLOW_UID=$(id -u)" > .env
+
 # Start Airflow using Docker Compose
+/usr/local/bin/docker-compose up airflow-init
+
 /usr/local/bin/docker-compose up -d
